4b17eeee037c50284354c650a3a204a3abc51fe7
Rodrigo Juanes <rodjuabu@gmail.com>
2024-10-14 18:32:13 +0200

    Merge branch 'main' of https://github.com/dragonsearch/Cajamar-UniversityHack-2024 Ramificación innecesaria. Sincronización de las ramas.

--- losca/tools/utils.py
+++ losca/tools/utils.py
@@ -4,7 +4,7 @@ import os
 
 
 
-def mean_values_biorreactor(bt, datetime_init, datetime_end, id_biorreactor, lot):
+def mean_values_biorreactor(bt, th, datetime_init, datetime_end, id_biorreactor, lot):
 
     #Get the data between the range of datetimes and matching the from id_biorreactor, transposing the dataframe
     bt_dttm_range = bt[(bt['f_h_bt'].astype(str).str[:-6] >= datetime_init) 
@@ -17,11 +17,14 @@ def mean_values_biorreactor(bt, datetime_init, datetime_end, id_biorreactor, lot
     #Adding lot column
     bt_mean_dttm_range['lote'] = lot
 
+    #Adding columns for temparture and humidity
+    bt_mean_dttm_range = pd.concat([bt_mean_dttm_range, t_h_biocent(th, datetime_init, datetime_end, 'biorreactor')], axis=1)
+
 
     return bt_mean_dttm_range
 
     
-def mean_values_centrifuga(ct, datetime_init, datetime_end, id_centrifuga, lot):
+def mean_values_centrifuga(ct, th, datetime_init, datetime_end, id_centrifuga, lot):
 
     #Get the data between the range of datetimes and matching the from id_biorreactor, transposing the dataframe
     ct_dttm_range = ct[(ct['f_h_ct'].astype(str).str[:-6] >= datetime_init) 
@@ -34,10 +37,13 @@ def mean_values_centrifuga(ct, datetime_init, datetime_end, id_centrifuga, lot):
     #Adding lot column
     ct_mean_dttm_range['lote'] = lot
 
+    #Adding columns for temparture and humidity
+    ct_mean_dttm_range = pd.concat([ct_mean_dttm_range, t_h_biocent(th, datetime_init, datetime_end, 'centrifuga')], axis=1)
+
     return ct_mean_dttm_range
 
 
-def mean_t_h_componentes(th, datetime_init, datetime_end):
+def mean_t_h_principal(th, datetime_init, datetime_end):
 
     #Get the data between the range of datetimes
     main_dock_th = th[((th['datetime_th'].astype(str).str[:-6] >= datetime_init)) 
@@ -45,12 +51,20 @@ def mean_t_h_componentes(th, datetime_init, datetime_end):
     
     #Mean values and storage in a dataframe with the same columns, name columns as the original dataframe
     main_dock_th_mean = pd.DataFrame(main_dock_th.mean(), index = main_dock_th.columns).transpose()
-
-    #Adding to dataframe the column for temperature and humidity according to transport to production dock, only columns for temperature and humidity
-    main_dock_th_mean['t_al_prod_th'] = th[(th['datetime_th'].astype(str).str[:-6] == datetime_end)]['t_al_prod_th'].values[0]
-    main_dock_th_mean['h_al_prod_th'] = th[(th['datetime_th'].astype(str).str[:-6] == datetime_end)]['h_al_prod_th'].values[0]
                                            
     return main_dock_th_mean
+
+def mean_t_h_produccion(th, datetime_init, datetime_end):
+
+    #Get the data between the range of datetimes
+    main_dock_th = th[((th['datetime_th'].astype(str).str[:-6] >= datetime_init)) 
+                        & (th['datetime_th'].astype(str).str[:-6] <= datetime_end)].loc[:,'t_al_prod_th':'h_al_prod_th']
+    
+    #Mean values and storage in a dataframe with the same columns, name columns as the original dataframe
+    main_dock_th_mean = pd.DataFrame(main_dock_th.mean(), index = main_dock_th.columns).transpose()
+                                           
+    return main_dock_th_mean
+
 
 
 def t_h_biocent(df, datetime_init, datetime_end, type):
